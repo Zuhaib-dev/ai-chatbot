afa15873c92b376747bee7b1b6fe59b565c903d7
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-07 12:16:20 +0530

    Add dotenv configuration and integrate Google GenAI service
    - Updated package.json and package-lock.json to include dotenv and @google/genai dependencies.
    - Created .env file for environment variables.
    - Set up server.js to use dotenv and handle socket events.
    - Implemented initial structure for ai.service.js to utilize GoogleGenAI.

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+require('dotenv').config()
 const app = require("./src/app");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, {
+  /* options */
+});
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("A user Connectd");
+  socket.on("disconnect", () => {
+    console.log("A user is dissconnectd");
+  });
+  socket.on("message", (data)=>{
+    console.log(data);
+    
+    console.log("A user send a message");
+    
+  })
 });
 httpServer.listen(3000, () => {
   console.log("server is running on port 3000");
---
+++ src/service/ai.service.js
@@ -0,0 +1,8 @@
+const { GoogleGenAI }  = require ("@google/genai");
+
+const ai = new GoogleGenAI({});
+
+async function generateResponse(prompt) {
+    
+    
+}
