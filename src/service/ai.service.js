61215b0c2dede9cd32d1e914da5320434def22ca
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-07 12:20:04 +0530

    Update .gitignore to include .env and implement response generation in ai.service.js

--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -3,6 +3,11 @@ const { GoogleGenAI }  = require ("@google/genai");
 const ai = new GoogleGenAI({});
 
 async function generateResponse(prompt) {
-    
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents:prompt
+    })
+    return response.text
     
 }
+module.exports = generateResponse
