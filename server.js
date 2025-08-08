0839911023360023a24b38d590997672bd5ce149
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-08 10:56:18 +0530

    Fix typos in server.js and ai.service.js for improved readability and functionality

--- server.js
+++ server.js
@@ -8,37 +8,41 @@ const io = new Server(httpServer, {
   /* options */
 });
 const chatHistory = [
-  {
-    role:"user",
-    parts:[{text:'who was pm of India in 2019'}]
-  },
-  {
-    role:"model",
-    parts:[
-      {
-        text:"The Prime Minister of India in 2019 was **Narendra Modi**"
-      }
-    ]
-  }
+
 ]
 
 io.on("connection", (socket) => {
-  console.log("A user Connectd");
-  socket.on("disconnect", () => {
-    console.log("A user is dissconnectd");
-  });
-  socket.on("ai-message", async (data) => {
-    console.log("Ai message recieved:", data);
-    chatHistory.push({
-      role:"user",
-      parts:[{text:data}]
+    console.log("A user connected")
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected")
+    });
+
+    /* ai-message */
+
+    socket.on('ai-message', async (data) => {
+        console.log("Ai message received:", data);
+
+        chatHistory.push({
+            role: "user",
+            parts: [ { text: data } ]
+        });
+
+        const mama = await generateResponse(chatHistory)
+
+        chatHistory.push({
+            role: "model",
+            parts: [ { text: mama } ]
+        });
+
+        socket.emit("ai-message-response", mama)
+
     })
 
-    const mama = await generateResponse(chatHistory);
-    console.log("AI-Response", mama);
-    socket.emit("ai-message-response", { mama });
-  });
+
+
 });
+
 httpServer.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+    console.log('Server is running on port 3000');
+})
--- src/service/ai.service.js
+++ src/service/ai.service.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({});
 async function generateResponse(chatHistory) {
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: chatHistroy,
+    contents: chatHistory,
   });
   return response.text;
 }
