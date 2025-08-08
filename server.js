bc5eac9c7cef67b9aec5c577ec90a2ab708fff1b
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-08 10:44:05 +0530

    Refactor AI message handling in server.js and update ai.service.js to use chat history for response generation

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const generateResponse = require("./src/service/ai.service");
 const io = new Server(httpServer, {
   /* options */
 });
+const chatHistory = [
+  {
+    role:"user",
+    parts:[{text:'who was pm of India in 2019'}]
+  },
+  {
+    role:"model",
+    parts:[
+      {
+        text:"The Prime Minister of India in 2019 was **Narendra Modi**"
+      }
+    ]
+  }
+]
 
 io.on("connection", (socket) => {
   console.log("A user Connectd");
@@ -15,8 +29,12 @@ io.on("connection", (socket) => {
   });
   socket.on("ai-message", async (data) => {
     console.log("Ai message recieved:", data);
+    chatHistory.push({
+      role:"user",
+      parts:[{text:data}]
+    })
 
-    const mama = await generateResponse(data);
+    const mama = await generateResponse(chatHistory);
     console.log("AI-Response", mama);
     socket.emit("ai-message-response", { mama });
   });
